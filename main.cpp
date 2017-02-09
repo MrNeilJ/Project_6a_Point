398c6d74f405c759ae8d794e790368b3f650a2db
Neil Johnson <mr.neil.j@gmail.com>
2017-02-09 00:56:23 -0800

    Adjusted values for the linesegment constructor

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Point.hpp"
+#include "LineSegment.hpp"
 
 int main() {
     Point p1(-1.5, 0.0);
@@ -15,5 +16,15 @@ int main() {
 
     std::cout << "Distance from p1 to p2 is: " << dist << std::endl;
     std::cout << "Distance from p3 to p4 is: " << p3.distanceTo(p4) << std::endl;
+
+
+    Point p5(4.3, 7.52);
+    Point p6(-17.0, 1.5);
+    LineSegment ls1(p5, p6);
+    double length = ls1.length();
+    double slope = ls1.slope();
+
+    std::cout <<"Length from p5 to p6 is: " << length << std::endl;
+    std::cout <<"Slope for p5 and p6 is:  " << slope << std::endl;
     return 0;
 }
