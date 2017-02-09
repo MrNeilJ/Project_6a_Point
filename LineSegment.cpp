0a1eea48a88af1d9f55eea439bceb5d4cd037020
Neil Johnson <mr.neil.j@gmail.com>
2017-02-09 00:19:05 -0800

    Added in definitions and prototypes for the new class and commented each function

--- LineSegment.cpp
+++ LineSegment.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "LineSegment.hpp"
+
+LineSegment::LineSegment() {
+
+}
+
+LineSegment::LineSegment(Point obj1, Point obj2) {
+
+}
+
+void LineSegment::setEnd1() {
+
+}
+
+void LineSegment::setEnd2() {
+
+}
+
+Point LineSegment::getEnd1() {
+    return Point();
+}
+
+Point LineSegment::getEnd2() {
+    return Point();
+}
+
+double LineSegment::length() {
+    return 0;
+}
+
+double LineSegment::slope() {
+    return 0;
+}
--- LineSegment.hpp
+++ LineSegment.hpp
@@ -6,7 +6,33 @@
 #define POINT_LINESEGMENT_HPP
 
 
+#include "Point.hpp"
+
 class LineSegment {
+private:
+    Point spot1;
+    Point spot2;
+
+public:
+    // CONSTRUCTORS
+    // Blank Constructor
+    LineSegment();
+    // Filled Constructor
+    LineSegment(Point obj1, Point obj2);
+
+    // Setter Functions
+    void setEnd1();
+    void setEnd2();
+
+    // Getter Functions
+    Point getEnd1();
+    Point getEnd2();
+
+    // Formula Functions
+    double length();
+    double slope();
+
+
 
 };
 
