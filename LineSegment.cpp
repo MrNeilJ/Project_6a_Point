5d53563973dfd37b474abbfd0dc13b89b30a282a
Neil Johnson <mr.neil.j@gmail.com>
2017-02-09 00:27:23 -0800

    Added more function definitions and filled them in to be functional

--- LineSegment.cpp
+++ LineSegment.cpp
@@ -3,35 +3,39 @@
 //
 
 #include "LineSegment.hpp"
+#include "Point.hpp"
 
 LineSegment::LineSegment() {
-
+    Point spot1(0,0);
+    Point spot2(0,0);
 }
 
 LineSegment::LineSegment(Point obj1, Point obj2) {
-
+    Point spot1(obj1.getXCoord(), obj1.getYCoord());
+    Point spot2(obj2.getXCoord(), obj2.getYCoord());
 }
 
-void LineSegment::setEnd1() {
-
+void LineSegment::setEnd1(Point input) {
+    spot1 = input;
 }
 
-void LineSegment::setEnd2() {
-
+void LineSegment::setEnd2(Point input) {
+    spot2 = input;
 }
 
 Point LineSegment::getEnd1() {
-    return Point();
+    return spot1;
 }
 
 Point LineSegment::getEnd2() {
-    return Point();
+    return spot2;
 }
 
 double LineSegment::length() {
-    return 0;
+    return spot1.distanceTo(spot2);
 }
 
+// FORMULA: m = (y2 - y1) / (x2 - x1)
 double LineSegment::slope() {
-    return 0;
+
 }
