6ba786e398955dbb40a103eaeb61c7f88500912c
neiljohnson <mr.neil.j@gmail.com>
2017-02-09 15:30:05 -0800

    Added header comments to each file

--- LineSegment.cpp
+++ LineSegment.cpp
@@ -1,7 +1,14 @@
-
-
-
-
+/**************************************************************
+ * Author: Neil Johnson
+ *
+ * Date: 2.10.2017
+ *
+ * Description: This is a class file for the class Point.
+ * In this file we host the application functions for the
+ * class.  These functions allow the user to create plot points
+ * and pull coordinates from previously made objects and the
+ * distance between two points.
+**************************************************************/
 #include "LineSegment.hpp"
 
 
--- Point.hpp
+++ Point.hpp
@@ -1,20 +1,14 @@
-/* Write a class called Point that contains two doubles that represent its x- and y-coordinates.
- * It should have get and set methods for both fields.  It should have a constructor that takes
- * two double parameters and passes those values to the set methods to initialize its fields.
- * It should have a default constructor that initializes both coordinates to 0 (also using the
- * set methods).  It should also contain a method called distanceTo that takes as a parameter a
- * constant reference to another Point and returns the distance from the Point that was passed as
- * a parameter to the Point that we called the method of. You will need to use sqrt(). For example
- * at the end of the following, dist should be equal to 5.0:
+/**************************************************************
+ * Author: Neil Johnson
  *
- * EXAMPLE:
- *  Point p1(-1.5, 0.0);
- *  Point p2(1.5, 4.0);
- *  double dist = p1.distanceTo(p2);
+ * Date: 2.10.2017
  *
- *  FORMULA NEEDED:
- *  d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
-*/
+ * Description: This is a class file for the class Point.
+ * In this file we host the application functions for the
+ * class.  These functions allow the user to create plot points
+ * and pull coordinates from previously made objects and the
+ * distance between two points.
+**************************************************************/
 
 #ifndef POINT_POINT_HPP
 #define POINT_POINT_HPP
