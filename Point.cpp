c4986bd8a7633ce1019b9592dc1a05670700336b
neiljohnson <mr.neil.j@gmail.com>
2017-02-08 16:12:42 -0800

    Added basic constructor functions

--- Point.cpp
+++ Point.cpp
@@ -1,5 +1,17 @@
-//
-// Created by Neil Johnson on 2/8/17.
-//
+/* Write a class called Point that contains two doubles that represent its x- and y-coordinates.
+ * It should have get and set methods for both fields.  It should have a constructor that takes
+ * two double parameters and passes those values to the set methods to initialize its fields.
+ * It should have a default constructor that initializes both coordinates to 0 (also using the
+ * set methods).  It should also contain a method called distanceTo that takes as a parameter a
+ * constant reference to another Point and returns the distance from the Point that was passed as
+ * a parameter to the Point that we called the method of. You will need to use sqrt(). For example
+ * at the end of the following, dist should be equal to 5.0:
+ *
+ * EXAMPLE:
+ *  Point p1(-1.5, 0.0);
+ *  Point p2(1.5, 4.0);
+ *  double dist = p1.distanceTo(p2);
+*/
 
 #include "Point.hpp"
+
