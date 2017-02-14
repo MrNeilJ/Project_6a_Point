d39c86f48be05dd3a7b2b742f1f549b8621b7803
neiljohnson <mr.neil.j@gmail.com>
2017-02-13 19:44:57 -0800

    Pulled more examples from the list in main

--- main.cpp
+++ main.cpp
@@ -5,17 +5,6 @@
 int main() {
     /*
 
-    double dist = p1.distanceTo(p2);
-
-    std::cout << "Distance from p1 to p2 is: " << dist << std::endl;
-    std::cout << "Distance from p3 to p4 is: " << p3.distanceTo(p4) << std::endl;
-
-
-    Point p5(4.3, 7.52);
-    Point p6(-17.0, 1.5);
-    LineSegment ls1(p5, p6);
-    double length = ls1.length();
-    double slope = ls1.slope();
 
     std::cout <<"Length from p5 to p6 is: " << length << std::endl;
     std::cout <<"Slope for p5 and p6 is:  " << slope << std::endl;
