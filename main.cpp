27851147f0c8d6e0e71eba4c5e5a74f262315d74
neiljohnson <mr.neil.j@gmail.com>
2017-02-13 19:44:38 -0800

    Removed some of the grayed out examples from earlier

--- main.cpp
+++ main.cpp
@@ -4,14 +4,6 @@
 
 int main() {
     /*
-    Point p1(-1.5, 0.0);
-    Point p2(1.5, 4.0);
-
-    Point p3;
-    p3.setXCoord(2.2);
-    p3.setYCoord(3.4);
-
-    Point p4(1.1 , 2.2);
 
     double dist = p1.distanceTo(p2);
 
