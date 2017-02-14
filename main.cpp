f04a90c083b22dd4e129a4e3ee0c4954bcaacf39
neiljohnson <mr.neil.j@gmail.com>
2017-02-13 19:45:21 -0800

    Cleaned up gray text comments

--- main.cpp
+++ main.cpp
@@ -6,20 +6,6 @@ int main() {
     /*
 
 
-    std::cout <<"Length from p5 to p6 is: " << length << std::endl;
-    std::cout <<"Slope for p5 and p6 is:  " << slope << std::endl;
-
-
-    Point p7(1.0,1.0);
-    Point p8(3.0, 3.0);
-
-    LineSegment ls2(p7, p8);
-
-    std::cout << "Length from p7 to p8 is: " << ls2.length() << std::endl;
-    std::cout << "Slope for p7 to p8 is: " << ls2.slope() << std::endl;
-
-    Point p9;
-
     LineSegment ls3(p9,p9);
     std::cout << "\n\nLength from p7 to p8 is: " << ls3.length() << std::endl;
     std::cout << "Slope for p7 to p8 is: " << ls3.slope() << std::endl;
